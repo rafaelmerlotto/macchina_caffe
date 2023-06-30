c8bb0bb43fff48c3c51128f37134f233a3c05410
Rafael Merlotto <rafaelmerlottogmail.com>
2023-06-30 15:56:46 +0200

    coding-caffe

--- index.js
+++ index.js
@@ -5,123 +5,118 @@ const avvisi = document.querySelector('.avvisi');
 const onOff = document.querySelector('.onOff');
 const riempireSerbatoio = document.querySelector('.riempireSerbatoio');
 const livelloSerbatoio = document.querySelector('.livelloSerbatoio');
-const cappuccino = document.querySelector('.cappuccino');
-const outputCaffe = document.querySelector('output-caffe')
+const cialda = document.querySelector('.cialda');
+const outputCaffe = document.querySelector('.output-caffe')
+const coffeeCorto = document.querySelector('.coffeeCorto');
+const coffeeLungo = document.querySelector('.coffeeLungo');
+const prendeCaffe = document.querySelector('.prendeCaffe');
+const tazza = document.querySelector('.tazza')
+livelloSerbatoio.innerText = 0;
 
- livelloSerbatoio.innerText = 0;
+tazza.addEventListener('click', () => {
+ setTimeout(() => {
+    coffeeCorto.style.visibility = 'hidden';
+    coffeeLungo.style.visibility = 'hidden';
+ }, 2000)
+   
+})
 
-const MacchinaCaffe = function (accesa, LivelloAcqua, livelloLatte) {
+const MacchinaCaffe = function (accesa, LivelloAcqua, cialdacaffe) {
     this.accesa = accesa;
     this.LivelloAcqua = LivelloAcqua;
-    this.livelloLatte = livelloLatte;
+    this.cialdacaffe = cialdacaffe;
 }
+
 
 MacchinaCaffe.prototype.accendi = function () {
     this.accesa = !this.accesa;
     onOff.style.backgroundColor = `${this.accesa ? 'green' : 'red'}`
-    console.log(`${this.accesa ? 'accesa' : 'spenta'}`)
-   
-
-
 }
+onOff.addEventListener('click', (event) => {
+    event.preventDefault();
+    macchinettaCaffe.accendi();
+})
+
 
 MacchinaCaffe.prototype.acquaSerbatoio = function () {
     if (this.LivelloAcqua <= 0) {
-        avvisi.innerText = "Il serbatoio d'acqua è vuoto";
+        avvisi.innerText = "Il serbatoio è vuoto";
         return this.LivelloAcqua = 0;
     }
-
 }
 
+
 MacchinaCaffe.prototype.riempeSebatoio = function () {
-   
     this.LivelloAcqua += 1;
-    if (this.LivelloAcqua >= 1) {
+    if (this.LivelloAcqua > 0) {
         avvisi.innerText = "";
         livelloSerbatoio.innerText = this.LivelloAcqua;
         livelloSerbatoio.style.backgroundColor = '#049ce8'
         if (this.LivelloAcqua >= 10) {
-            avvisi.innerText = 'Il serbatoio è pieno';
             return this.LivelloAcqua = 9;
         }
     }
-    console.log(this.LivelloAcqua)
 }
-
-MacchinaCaffe.prototype.latte = function () { 
-  
- if(this.accesa && this.livelloLatte){
-    setTimeout(() => {
-        avvisi.innerText = "Cappuccino ☕";
-        console.log(this.livelloLatte)
-    }, 3000) 
- }
-}
-
-MacchinaCaffe.prototype.caffeCorto = function () {
-
-    if (this.accesa && this.LivelloAcqua) {
-        setTimeout(() => {
-            outputCaffe.innerText = "Caffe corto ☕"
-        livelloSerbatoio.innerText = this.LivelloAcqua -= 1;
-    
-            macchinettaCaffe.acquaSerbatoio();
-
-            console.log(this.LivelloAcqua)
-
-        }, 3000);
-       
-    }
-}
-
-MacchinaCaffe.prototype.caffeLungo = function () {
-    if (this.accesa && this.LivelloAcqua) {
-        setTimeout(() => {
-            avvisi.innerText = "Caffe Lungo ☕"
-            livelloSerbatoio.innerText = this.LivelloAcqua -= 1;
-    
-
-            macchinettaCaffe.acquaSerbatoio();
-
-
-
-            console.log(this.LivelloAcqua)
-
-
-        }, 3000);
-    }
-}
-
-
-caffeCorto.addEventListener('click', () => {
-   
-        macchinettaCaffe.caffeCorto(); 
-})
-
-caffeLungo.addEventListener('click', () => {
-    macchinettaCaffe.caffeLungo();
-
-})
-
-onOff.addEventListener('click', (event) => {
-    event.preventDefault();
-    macchinettaCaffe.accendi();
-
-})
-
 riempireSerbatoio.addEventListener('click', () => {
     macchinettaCaffe.riempeSebatoio();
 })
 
-cappuccino.addEventListener('click', () => {
-         macchinettaCaffe.latte();
+
+MacchinaCaffe.prototype.cialda = function () {
+    if (this.accesa) {
+        this.cialdacaffe = !this.cialdacaffe;
+        cialda.style.backgroundColor = `${this.cialdacaffe ? '#6f4e37' : '#9ea8b4'}`
+        //console.log(`${this.cialdacaffe ? 'Inserita' : 'tolta'}`)
+    }
+}
+cialda.addEventListener('click', (event) => {
+    event.preventDefault();
+    macchinettaCaffe.cialda();
 })
 
 
-const macchinettaCaffe = new MacchinaCaffe(false, 0, 2);
 
+MacchinaCaffe.prototype.caffeCorto = function () {
+    if (this.accesa && this.cialdacaffe && this.LivelloAcqua) {
+        setTimeout(() => {
+
+            livelloSerbatoio.innerText = this.LivelloAcqua -= 1;
+            macchinettaCaffe.acquaSerbatoio();
+            macchinettaCaffe.cialda();
+            coffeeCorto.style.visibility ='visible';
+            coffeeCorto.removeAttribute('hidden');
+        }, 3000);
+    }
+    if (this.cialdacaffe === false) {
+        cialda.style.backgroundColor = '#9ea8b4';
+    }
+}
+caffeCorto.addEventListener('click', () => {
+    macchinettaCaffe.caffeCorto();
+
+})
+
+
+MacchinaCaffe.prototype.caffeLungo = function () {
+    if (this.accesa && this.cialdacaffe && this.LivelloAcqua) {
+        setTimeout(() => {
+
+            livelloSerbatoio.innerText = this.LivelloAcqua -= 1;
+            macchinettaCaffe.acquaSerbatoio();
+            macchinettaCaffe.cialda();
+            coffeeLungo.style.visibility ='visible';
+            coffeeLungo.removeAttribute('hidden');
+        }, 3000);
+    }
+    if (this.cialdacaffe === false) {
+        cialda.style.backgroundColor = '#9ea8b4';
+    }
+}
+caffeLungo.addEventListener('click', () => {
+    macchinettaCaffe.caffeLungo();
+})
+
+
+const macchinettaCaffe = new MacchinaCaffe(false, 0, false);
 macchinettaCaffe.acquaSerbatoio();
-
-
-
 
